953d367deddf6ba2be5b23ca552e06e9a5c02b8a
jrwatson_utah_edu <james.r.watson@utah.edu>
2017-07-12 10:11:58 -0600

    Attempt tp refresh for PYpi

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ from setuptools import setup
 
 setup(name='marchhare',
       version='2017.06.25',
-      description='Module ARCHive for a Hobby and Research Environment',
-      url='https://bitbucket.org/jwatson_utah_edu/marchhare/raw/master/dist/marchhare-2017.6.25.tar.gz',
+      description='Module ARCHive for a Hobby And Research Environment',
+      url='http://bitbucket.org/jwatson_utah_edu/marchhare/raw/master/dist/marchhare-2017.6.25.tar.gz',
       author='James Watson',
       author_email='james.r.watson@utah.edu',
       license='MIT',
       packages=['marchhare'],
-      zip_safe=False)
+      zip_safe=True)
