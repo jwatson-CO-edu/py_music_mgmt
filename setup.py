8e412ea024bbcd768a88f25f45a33d1b163932f7
jrwatson_utah_edu <james.r.watson@utah.edu>
2017-07-12 08:33:00 -0600

    updated dist with a new URL

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 setup(name='marchhare',
       version='2017.06.25',
       description='Module ARCHive for a Hobby and Research Environment',
-      url='https://bitbucket.org/jwatson_utah_edu/marchhare',
+      url='https://bitbucket.org/jwatson_utah_edu/marchhare/raw/master/dist/marchhare-2017.6.25.tar.gz',
       author='James Watson',
       author_email='james.r.watson@utah.edu',
       license='MIT',
