0409a2a99cc800a5a009d4fed141c9e1fcb0e447
James Watson <jawa5023@colorado.edu>
2019-08-24 17:24:13 -0600

    Updated the player to accept CLI args

--- temp_player.py
+++ temp_player.py
@@ -11,37 +11,49 @@ PARENTDIR = os.path.dirname( SOURCEDIR )
 def prepend_dir_to_path( pathName ): sys.path.insert( 0 , pathName ) # Might need this to fetch a lib in a parent directory
 prepend_dir_to_path( SOURCEDIR )
 from marchhare.marchhare import get_EXT
-print "Loaded Libs!"
 
-MUSICDIR = os.path.join( "/media/" , getpass.getuser() , "MUSIC/Music" ) 
-print "The music directory is" , MUSICDIR , "but is it valid?" , os.path.isdir( MUSICDIR )
+_DEBUG = False
 
-tracklist = []
-
-# https://www.pythoncentral.io/how-to-traverse-a-directory-tree-in-python-guide-to-os-walk/
-for dirName , subdirList , fileList in os.walk( MUSICDIR ):
-    for fName in fileList:
-        if get_EXT( fName ) == "MP3":
-            tracklist.append( os.path.join( dirName , fName ) )
-            
-print "Found" , len( tracklist ) , "playable files! For example" , choice( tracklist )
-
-N_tracks = 20
-
-for i in range( N_tracks ):
-    track = choice( tracklist )
-    print i+1 , "of" , N_tracks , ", Now playing" , track , 
-    player = vlc.MediaPlayer( track )
-    print ", Duration:" , player.get_length() / 1000.0 , 
-    player.play()
-    time.sleep( 0.5 )
-    Ended = 6
-    current_state = player.get_state()
-    while current_state != Ended:
-        print '.',
-        sys.stdout.flush()
-        time.sleep( 1.0 )
-        current_state = player.get_state() 
-    print
+if __name__ == '__main__':
+    if _DEBUG: print "Script called with arguments: ", sys.argv
     
-print "End!"
+    # Set the number of tracks to play to the number specified by the user or the default value
+    try:
+        N_tracks = int( sys.argv[1] )
+    except:
+        N_tracks = 20
+    
+    # Construct the music directory
+    MUSICDIR = os.path.join( "/media/" , getpass.getuser() , "MUSIC/Music" ) 
+    print "The music directory is" , MUSICDIR , "but is it valid?" , os.path.isdir( MUSICDIR )
+    
+    # Walk the directory and build a list of all playable music files
+    tracklist = []
+    # https://www.pythoncentral.io/how-to-traverse-a-directory-tree-in-python-guide-to-os-walk/
+    for dirName , subdirList , fileList in os.walk( MUSICDIR ):
+        for fName in fileList:
+            if get_EXT( fName ) == "MP3":
+                tracklist.append( os.path.join( dirName , fName ) )
+                
+    print "Found" , len( tracklist ) , "playable files! For example" , choice( tracklist )
+    
+    # For each track, pick a random song, print title, and play it while printing status dots
+    for i in range( N_tracks ):
+        track = choice( tracklist )
+        print i+1 , "of" , N_tracks , ", Now playing" , track , 
+        player = vlc.MediaPlayer( track )
+        print ", Duration:" , player.get_length() / 1000.0 , 
+        player.play()
+        time.sleep( 0.5 )
+        Ended = 6
+        current_state = player.get_state()
+        # Check that the song is still playing at intervals, if so, print a status dot
+        while current_state != Ended:
+            print '.',
+            sys.stdout.flush()
+            time.sleep( 1.0 )
+            current_state = player.get_state() 
+        # Song over, choose next 
+        print
+        
+    print "End!"
